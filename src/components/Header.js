d8801ead3a58850dae220eb5efc5e8a94f252984
Javier Ezpeleta <javierezpeleta@gmail.com>
2017-09-19 12:29:52 +0200

    tried some SVG morphing.. worked great.. now back to normal again!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 import LogoImage from './../images/readable-logo.png'
 import { Link } from 'react-router-dom'
 import AddPostButton from './post/AddPostButton'
+class Header extends Component {
+  render() {
+    return (
+      <div className="readable-header">
+        <div className="container ">
+          <nav className="navbar ">
+            <div className="navbar-brand">
+              <Link className="navbar-item" to="/">
+                <img
+                  src={LogoImage}
+                  alt="Bulma: a modern CSS framework based on Flexbox"
+                  width="112"
+                  height="26"
+                />
+              </Link>
+              <a
+                className="navbar-item is-hidden-desktop"
+                href="https://github.com/JaviEzpeleta"
+              >
+                <span className="icon">
+                  <i className="fa fa-github" />
+                </span>
+              </a>
 
-const Header = () => (
-  <div className="readable-header">
-    <div className="container ">
-      <nav className="navbar ">
-        <div className="navbar-brand">
+              <a
+                className="navbar-item is-hidden-desktop"
+                href="https://twitter.com/ezpe"
+              >
+                <span className="icon">
+                  <i className="fa fa-twitter" />
+                </span>
+              </a>
 
-          <Link className="navbar-item" to="/">
-            <img src={LogoImage} alt="Bulma: a modern CSS framework based on Flexbox"
-              width="112" height="26"/>
-          </Link>
-          <a className="navbar-item is-hidden-desktop" href="https://github.com/JaviEzpeleta">
-            <span className="icon">
-              <i className="fa fa-github"></i>
-            </span>
-          </a>
-
-          <a className="navbar-item is-hidden-desktop" href="https://twitter.com/ezpe">
-            <span className="icon">
-              <i className="fa fa-twitter"></i>
-            </span>
-          </a>
-
-          <div className="navbar-burger burger" data-target="navMenuDocumentation">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-
-        <div id="navMenuDocumentation" className="navbar-menu">
-          <div className="navbar-end">
-            <a className="navbar-item is-hidden-desktop-only" href="https://github.com/JaviEzpeleta">
-              Github
-            </a>
-            <a className="navbar-item is-hidden-desktop-only" href="https://twitter.com/ezpe">
-              Twitter
-            </a>
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <AddPostButton />
-                </p>
+              <div
+                className="navbar-burger burger"
+                data-target="navMenuDocumentation"
+              >
+                <span />
+                <span />
+                <span />
               </div>
             </div>
-          </div>
+
+            <div id="navMenuDocumentation" className="navbar-menu">
+              <div className="navbar-end">
+                <a
+                  className="navbar-item is-hidden-desktop-only"
+                  href="https://github.com/JaviEzpeleta"
+                >
+                  Github
+                </a>
+                <a
+                  className="navbar-item is-hidden-desktop-only"
+                  href="https://twitter.com/ezpe"
+                >
+                  Twitter
+                </a>
+                <div className="navbar-item">
+                  <div className="field is-grouped">
+                    <p className="control">
+                      <AddPostButton />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
         </div>
-      </nav>
-
-    </div>
-  </div>
-)
-
+      </div>
+    )
+  }
+}
 export default Header
